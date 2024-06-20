b1a64bb1c05a8ed25e8000c0025ac491a8c4d2a5
Sumanth <93563303+Sumanth484@users.noreply.github.com>
2024-06-20 16:40:11 +0530

    Update main.tf

--- Terraform-Modules/main.tf
+++ Terraform-Modules/main.tf
@@ -1,5 +1,6 @@
 
 module "ec2_instance" {
+  #source can be another directory,git repo or terraform registry path
   source = "git::https://github.com/iam-veeramalla/terraform-zero-to-hero.git//Day-3/modules/ec2_instance"
   ami_value = "ami-04b70fa74e45c3917"
   instance_type_value = "t2.micro"
